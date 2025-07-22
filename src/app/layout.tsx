8db146e9d7647c4aca7153d7ec13eeb659024d45
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-07-22 14:30:57 -0600

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from "next";
 import { CssBaseline } from "@mui/material";
+import { Playfair_Display } from "next/font/google";
+import localFont from "next/font/local";
+import clsx from "clsx";
 
 import "./global.scss";
 
@@ -7,13 +10,26 @@ export const metadata: Metadata = {
   title: "Nuestra boda | Ingrid y Mauricio",
 };
 
+const playfairDisplay = Playfair_Display({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  preload: true,
+});
+
+const swagume = localFont({
+  src: "../assets/fonts/Swagume.otf",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={clsx(playfairDisplay.className, swagume.className)}
+    >
       <body>
         <CssBaseline />
         {children}
