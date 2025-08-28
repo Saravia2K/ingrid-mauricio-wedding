ba27b2e73c28fa8469ee0e67aa0a9401f9889d38
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-08-28 08:27:09 -0600

    [Global] | Removing css attr use to be compatible with firefox

--- src/components/common/floral-arrangements/index.tsx
+++ src/components/common/floral-arrangements/index.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import { type PropsWithChildren } from "react";
 import Image from "next/image";
 import clsx from "clsx";
+import { Box, useTheme } from "@mui/material";
 
 import styles from "./flora-arrangements.module.scss";
 import lFlower from "@/assets/images/arrangements/Flores_Izquierda.svg";
 import rFlower from "@/assets/images/arrangements/Flores_Derecha.svg";
 
 export default function FloralArrangements(props: FloralArrangementsProps) {
+  const theme = useTheme();
+  const tabletMediaQuery = theme.breakpoints.up(768);
+
   const { children, containerClassName, flowers } = props;
+
+  const getFlowerSize = (size?: number | string) =>
+    size == undefined
+      ? undefined
+      : `${typeof size == "number" ? `${size}%` : size} !important`;
 
   return (
     <div
@@ -18,21 +29,25 @@ export default function FloralArrangements(props: FloralArrangementsProps) {
     >
       {flowers.map((f, i) => {
         const flower = f.position == "l" ? lFlower : rFlower;
+        const cssPosition = { l: "left", r: "right" }[f.position];
 
         return (
-          <div
+          <Box
             key={i}
-            className={clsx(styles.flower, {
-              [`${styles.l}`]: f.position == "l",
-              [`${styles.r}`]: f.position == "r",
-            })}
-            data-m-x={f.mobile?.x || 0}
-            data-m-y={f.mobile?.y}
-            data-t-x={f.tablet?.x || 0}
-            data-t-y={f.tablet?.y}
+            className={styles.flower}
+            sx={{
+              top: `${f.mobile?.y || 0}rem`,
+              [`${cssPosition}`]: `${f.mobile?.x || 0}rem`,
+              width: getFlowerSize(f.mobile?.size),
+              [`${tabletMediaQuery}`]: {
+                top: `${f.tablet?.y || f.mobile?.y || 0}rem`,
+                [`${cssPosition}`]: `${f.tablet?.x || 0}rem`,
+                width: getFlowerSize(f.tablet?.size),
+              },
+            }}
           >
             <Image src={flower} alt="Flower" />
-          </div>
+          </Box>
         );
       })}
       {children}
@@ -52,4 +67,5 @@ type FloralArrangementsProps = PropsWithChildren<{
 type FloralArrangementsCords = {
   x?: number;
   y: number;
+  size?: number | string;
 };
