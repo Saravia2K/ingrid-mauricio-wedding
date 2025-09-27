6a617445d65a15f67bdbc5f7bad33061d682ceb1
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-09-27 12:54:13 -0600

    [Invitation] | using main component and splitting page content

---
+++ src/app/invitacion/[guest-slug]/content.tsx
@@ -0,0 +1,54 @@
+import fs from "fs";
+import path from "path";
+
+import Main from "@/components/common/main";
+import MainBanner from "@/components/sections/main-banner";
+import Date from "@/components/sections/date";
+import SongPlayer from "@/components/sections/song-player";
+import History from "@/components/sections/history";
+import VideoPlayer from "@/components/sections/video-player";
+import Gallery from "@/components/sections/gallery";
+import Details from "@/components/sections/details";
+import Reminder from "@/components/sections/reminder";
+import Agenda from "@/components/sections/agenda";
+import GiftsAdvice from "@/components/sections/gifts-advice";
+import ChildrenAdvice from "@/components/sections/children-advice";
+import Confirmation from "@/components/sections/confirmation";
+
+import DATA from "@/assets/json/data.json";
+import { Guest } from "@/types/guest";
+
+const { song, videoUrl } = DATA;
+
+const historyPath = path.join(process.cwd(), "/src/history.txt");
+const history = fs.readFileSync(historyPath, {
+  encoding: "utf-8",
+  flag: "r",
+});
+
+export default async function InvitationPageContent({
+  companions,
+  guest,
+}: InvitationPageProps) {
+  return (
+    <Main>
+      <MainBanner />
+      <Date />
+      <SongPlayer songSrc={song.url} name={song.name} author={song.author} />
+      <History history={history} />
+      <VideoPlayer videoSrc={videoUrl} />
+      <Gallery />
+      <Details />
+      <Reminder />
+      <Agenda />
+      <GiftsAdvice />
+      <ChildrenAdvice />
+      <Confirmation leader={guest} companions={companions} />
+    </Main>
+  );
+}
+
+type InvitationPageProps = {
+  guest: Guest;
+  companions: Guest[];
+};
--- src/app/invitacion/[guest-slug]/page.tsx
+++ src/app/invitacion/[guest-slug]/page.tsx
@@ -1,26 +1,10 @@
 import { redirect } from "next/navigation";
 import { ResolvingMetadata, type Metadata } from "next";
 import prisma from "@/lib/prisma";
-import fs from "fs";
-import path from "path";
 
-import MainBanner from "@/components/sections/main-banner";
-import Date from "@/components/sections/date";
-import SongPlayer from "@/components/sections/song-player";
-import History from "@/components/sections/history";
-import VideoPlayer from "@/components/sections/video-player";
-import Gallery from "@/components/sections/gallery";
-import Details from "@/components/sections/details";
-import Reminder from "@/components/sections/reminder";
-import Agenda from "@/components/sections/agenda";
-import GiftsAdvice from "@/components/sections/gifts-advice";
-import ChildrenAdvice from "@/components/sections/children-advice";
-import Confirmation from "@/components/sections/confirmation";
+import InvitationPageContent from "./content";
 
-import DATA from "@/assets/json/data.json";
-
-import styles from "./invitacion.module.scss";
-
+//#region Metadata
 export async function generateMetadata(
   { params }: InvitationPageProps,
   parent: ResolvingMetadata
@@ -38,16 +22,11 @@ export async function generateMetadata(
     title: `${guest?.name} | ${parentInfo.title?.absolute}`,
   };
 }
-
-const { song, videoUrl } = DATA;
-const historyPath = path.join(process.cwd(), "/src/history.txt");
-const history = fs.readFileSync(historyPath, {
-  encoding: "utf-8",
-  flag: "r",
-});
+//#endregion
 
 const goToSinInvitar = () => redirect("/sin-invitar");
 export default async function InvitationPage({ params }: InvitationPageProps) {
+  // Cargando invitación
   const { "guest-slug": slug } = await params;
   const guest = await prisma.guest.findFirst({
     where: {
@@ -55,8 +34,11 @@ export default async function InvitationPage({ params }: InvitationPageProps) {
     },
   });
 
+  // ¿No existe? No está invitado
   if (!guest) return goToSinInvitar();
 
+  // ¿Es acompañante? Entonces se redirige a la invitación
+  // del verdadero invitado
   if (!guest.leader) {
     const leader = await prisma.guest.findFirst({
       where: {
@@ -70,6 +52,7 @@ export default async function InvitationPage({ params }: InvitationPageProps) {
     return redirect(`/invitacion/${leader.slug}`);
   }
 
+  // Se cargan los acompañantes del invitado
   const companions = await prisma.guest.findMany({
     where: {
       groupId: guest.groupId,
@@ -77,22 +60,7 @@ export default async function InvitationPage({ params }: InvitationPageProps) {
     },
   });
 
-  return (
-    <main className={styles.main}>
-      <MainBanner />
-      <Date />
-      <SongPlayer songSrc={song.url} name={song.name} author={song.author} />
-      <History history={history} />
-      <VideoPlayer videoSrc={videoUrl} />
-      <Gallery />
-      <Details />
-      <Reminder />
-      <Agenda />
-      <GiftsAdvice />
-      <ChildrenAdvice />
-      <Confirmation leader={guest} companions={companions} />
-    </main>
-  );
+  return <InvitationPageContent guest={guest} companions={companions} />;
 }
 
 type InvitationPageProps = {
