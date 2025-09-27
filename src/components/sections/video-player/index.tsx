d891d3d2040e51ef8f437f7e2b3b9d4de9bf77f0
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-09-27 12:58:29 -0600

    video muted by default

--- src/components/sections/video-player/index.tsx
+++ src/components/sections/video-player/index.tsx
@@ -29,7 +29,7 @@ export default function VideoPlayer({ videoSrc }: VideoPlayerProps) {
           <FaPlay className={styles["play-icon"]} />
         </div>
       )}
-      <video controls={!firstPlay} ref={videoRef}>
+      <video controls={!firstPlay} ref={videoRef} muted>
         <source src={videoSrc} type="video/mp4" />
         Tu navegador no soporta videos
       </video>
