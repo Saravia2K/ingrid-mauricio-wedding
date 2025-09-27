f68e438fb2411a481467d3f2ae002f2cb0f417e8
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-09-27 13:10:46 -0600

    [Gallery] | removing gallery, only showing preview

--- src/components/sections/gallery/index.tsx
+++ src/components/sections/gallery/index.tsx
@@ -1,12 +1,33 @@
-import path from "path";
-import fs from "fs";
-import GalleryClient from "./client";
+"use client";
 
-export default async function Gallery() {
-  const photosPath = path.join(process.cwd(), "/src/assets/images/photos");
-  const photosNames = fs.readdirSync(photosPath);
-  const photosLoaded = await Promise.all(
-    photosNames.map(async (p) => await import(`@/assets/images/photos/${p}`))
+import Image from "next/image";
+
+import styles from "./gallery.module.scss";
+import img1 from "@/assets/images/photos/IMG_9963.jpg";
+import img2 from "@/assets/images/photos/IMG_0051.jpg";
+import img3 from "@/assets/images/photos/IMG_0097.jpg";
+import img4 from "@/assets/images/photos/IMG_0072.jpg";
+import img5 from "@/assets/images/photos/IMG_0112.jpg";
+import img6 from "@/assets/images/photos/IMG_0095.jpg";
+import img7 from "@/assets/images/photos/9X1C6140.jpg";
+
+const IMAGES_LINE1 = [img1, img2, img3];
+const IMAGES_LINE2 = [img4, img5, img6, img7];
+export default function Gallery() {
+  return (
+    <section id="gallery" className={styles.gallery}>
+      <div className={styles.preview}>
+        <div className={`${styles.line} ${styles.line1}`}>
+          {IMAGES_LINE1.map((img, i) => (
+            <Image key={i} src={img} alt="" priority />
+          ))}
+        </div>
+        <div className={`${styles.line} ${styles.line2}`}>
+          {IMAGES_LINE2.map((img, i) => (
+            <Image key={i} src={img} alt="" priority />
+          ))}
+        </div>
+      </div>
+    </section>
   );
-  return <GalleryClient photosUrls={photosLoaded.map((p) => p.default)} />;
 }
